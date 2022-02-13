dd6579b75c84309d5458b2406e076236709e989f
Artem Koroliuk <artemkoroliuk86@gmail.com>
2022-02-13 18:14:57 +0200

    1

--- app.js
+++ app.js
@@ -1,48 +1,20 @@
-/*jshint esversion: 6 */
-console.log("hello world");
-//output for var 3 3 3
-/*for (var i=0; i<3; i++){
-    setTimeout(function(){
-        console.log(i);
-    });
-}
-for (let i=0; i<3; i++){
-    setTimeout (function(){
-        console.log(i);
-    });
-}
-const fun=function(a,b){
-    return a+b;
-}
-let a=5;
-console.log(a**2);
-console.log(fun(10,20));
-console.log("12"+12);
-console.log("12"-12);
-console.log(+"12"+12);*/
-const fun=function(a,b){
-    return a+b;
-}
-//home work
-//1.
-console.log("a"+fun**2+"a"+"s");
-//2.
-const calculator=function(a,b,c){
-    switch(c){
-        case "+":
-            return a+b; break;
-        case "-":
-            return a-b; break;
-        case "*":
-            return a*b; break;
-        case "/":
-            return a/b; break;
+function concatinate(prefix) {
+    return function (fix) {
+        return prefix+fix;
     }
 }
-console.log(calculator(100,2,"/"));
-const funy=function(5){function(5,10){
-    
-}}
+const concatApp = concatinate('App - ');
+const concatMessage = concatApp('Test status: Done');
+console.log(concatMessage);
 
 
+function getRandomNumber(min,max){
+    if(min>max){
+        return max+Math.floor(Math.random()*(min-max))
+    }
+    else {
+        return min+Math.floor(Math.random()*(max-min))
+    }
+}
+console.log(getRandomNumber(10,11))
 
